b67249cc4963eeb8e62c7fea84e7d0c7d59a1fd9
h2joh33 <h2joh33@gmail.com>
2016-04-05 15:32:02 +0900

    no message

--- src/main.f90
+++ src/main.f90
@@ -1,4 +1,18 @@
 PROGRAM VCSpractice
+    USE Util
+    
+    IMPLICIT NONE
+    
+    TYPE(neutron) :: nst
+    INTEGER :: i, icyc
+    INTEGER :: nht=10000
+    INTEGER :: ncyc=150, ninact=50
+    INTEGER :: nact
+    LOGICAL :: refl(4)
+    
+    nact=ncyc-ninact
+    refl = .TRUE.
+    
     
     
     
