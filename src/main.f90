6705b59866d989da4a5168616688dec68abe53f5
h2joh33 <h2joh33@gmail.com>
2016-04-05 15:32:23 +0900

    Revert "no message"
    This reverts commit b67249cc4963eeb8e62c7fea84e7d0c7d59a1fd9.

--- src/main.f90
+++ src/main.f90
@@ -1,18 +1,4 @@
 PROGRAM VCSpractice
-    USE Util
-    
-    IMPLICIT NONE
-    
-    TYPE(neutron) :: nst
-    INTEGER :: i, icyc
-    INTEGER :: nht=10000
-    INTEGER :: ncyc=150, ninact=50
-    INTEGER :: nact
-    LOGICAL :: refl(4)
-    
-    nact=ncyc-ninact
-    refl = .TRUE.
-    
     
     
     
